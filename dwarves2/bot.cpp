92b68c2a5a553738121d391308f98ee27b9fde52
Jhe0031 <jackie.he8@gmail.com>
2019-05-08 14:35:37 -0400

    Update

--- dwarves2/bot.cpp
+++ dwarves2/bot.cpp
@@ -20,32 +20,15 @@ const int MAX_NUM = 10;
 int ROWS;  // global variables
 int COLS;
 int NUM;
-bool start = true;
-bool up, down, lefty, righty, startB = false;
+int enough_wood = 300;
 
-bool isNextToATree(Dwarf & dwarf, int r, int c){
-  if (dwarf.look(r, c+1) == PINE_TREE || dwarf.look(r, c+1) == APPLE_TREE)
-    return true;
-  if (dwarf.look(r, c-1) == PINE_TREE || dwarf.look(r, c-1) == APPLE_TREE)
-    return true;
-  if (dwarf.look(r+1, c) == PINE_TREE || dwarf.look(r+1, c) == APPLE_TREE)
-    return true;
-  if (dwarf.look(r-1, c) == PINE_TREE || dwarf.look(r-1, c) == APPLE_TREE)
-    return true;
-  return false;
-}
+enum Section {CHOPWOOD, BUILDFENCE};
+Section current_section = CHOPWOOD;
 
-bool isNextToAFence(Dwarf & dwarf, int r, int c){
-  if (dwarf.look(r, c+2) == FENCE)
-    return true;
-  if (dwarf.look(r, c-2) == FENCE)
-    return true;
-  if (dwarf.look(r+2, c) == FENCE)
-    return true;
-  if (dwarf.look(r-2, c) == FENCE)
-    return true;
-  return false;
-}
+int setup_check[] = {0, 0, 0, 0, 0, 0};
+enum Build_Section {WALK_BS, BUILD_BS, SETUP_BS};
+Build_Section current_bs = SETUP_BS;
+
 /* onStart: 
 An Initialization procedure called at the start of the game.
 You can use it to initialize certain global variables, or do 
@@ -74,199 +57,145 @@ Parameters:
     minutes: number of minutes (0-59)
     log:     a cout-like log  */
 
+bool isNextToATree (Dwarf &dwarf, int r, int c) {
+  if ((dwarf.look(r, c) == PINE_TREE) || (dwarf.look(r, c) == APPLE_TREE)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 void onAction(Dwarf &dwarf, int day, int hours, int minutes, ostream &log) {
   // Get current position of the dwarf
   int r = dwarf.row();
   int c = dwarf.col();
+  // First phase: Collect enough wood to build the fences
+  // Second phase: Build at least 30 fences
 
-  if(startB && r == 10 && c == 10){
-    dwarf.start_build(NORTH);
-    startB = false;
-    return;
+  // Switch Section
+  if (dwarf.lumber() > enough_wood) {
+    log << "Building fences" << endl;
+    current_section = BUILDFENCE; // keep collecting wood until goal is reached
   }
-  
-  if(dwarf.look(r+1,c) == FENCE){
-    dwarf.start_walk(r-1,c);
-    // up = false;
-    return;
-  }else if(dwarf.look(r-1,c) == FENCE){
-    dwarf.start_walk(r+1,c);
-    // down = false;
-    return;
-  }else if(dwarf.look(r,c+1) == FENCE){
-    dwarf.start_walk(r,c-1);
-    // lefty = false;
-    return;
-  }else if(dwarf.look(r,c-1) == FENCE){
-    dwarf.start_walk(r,c+1);
-    // righty = false;
-    return;
-  }
-  
-  // Look if there is a tree next to the dwarf
-  if (isNextToATree(dwarf, r, c)) {
-    log << "Found a tree -- chop" << endl;
-    // If there is a pine tree, chop it
-    if (dwarf.look(r, c+1) == PINE_TREE || dwarf.look(r, c+1) == APPLE_TREE){
-      dwarf.start_chop(EAST);
-      return;
-    }      
-    if (dwarf.look(r, c-1) == PINE_TREE || dwarf.look(r, c-1) == APPLE_TREE){
-      dwarf.start_chop(WEST);
-      return;
-    }     
-    if (dwarf.look(r+1, c) == PINE_TREE || dwarf.look(r+1, c) == APPLE_TREE){
-      dwarf.start_chop(SOUTH);
-      return;
-    }      
-    if (dwarf.look(r-1, c) == PINE_TREE || dwarf.look(r-1, c) == APPLE_TREE){
-      dwarf.start_chop(NORTH);
-      return;
-    } 
-  }
-  else {   
-    int rr, cc;
-    //Instead move to where a tree is nearby
-
-    /**Need to mod the additions because it will go out of bounds and then
-    choose a random location I use 3 if statements to look in terms of
-    columns, rows then both rows and cols
-
-    **/
-    for(int i = 1; i < ROWS; i++){
-
-      if(isNextToATree(dwarf, (r+i) % ROWS, (c+i) % COLS) &&
-   !(dwarf.look((r+i) % ROWS, (c+i) % COLS) == PINE_TREE ||
-     dwarf.look((r+i) % ROWS, (c+i) % COLS) == APPLE_TREE)){
-  rr = (r + i) % ROWS;
-  cc = (c + i) % COLS;
-  log << "Walk to " << rr << " " << cc << endl;
-  dwarf.start_walk(rr,cc);
-  return;
-      }     
-
-      if(isNextToATree(dwarf, r, (c+i) % COLS) &&
-   !(dwarf.look(r, (c+i) % COLS) == PINE_TREE ||
-     dwarf.look(r, (c+i) % COLS) == APPLE_TREE)){
-  rr = r;
-  cc = (c + i) % COLS;
-  log << "Walk to " << rr << " " << cc << endl;
-  dwarf.start_walk(rr,cc);
-  return;
-      }
-
-      if(isNextToATree(dwarf, (r+i) % ROWS, c) &&
-   !(dwarf.look((r+i) % ROWS, c) == PINE_TREE ||
-     dwarf.look((r+i) % ROWS, c) == APPLE_TREE)){
-  rr = (r + i) % ROWS;
-  cc = c;
-  log << "Walk to " << rr << " " << cc << endl;
-  dwarf.start_walk(rr,cc);
-  return;
-      }
- 
-    }
-
-    //After all trees are cut down
-    /**
-       Dwarves will have to start building. The first dwarf with 50 lumber will start a fence
-       and all the dwarves will only build fences near that fence. So going to write a function that
-       will detect a fence location.
-     **/
-
-    if(dwarf.lumber() < 10){
-      rr = rand() % ROWS + 30;
-      cc = rand() % COLS + 30;
-      log << "Moving away" << endl;
-      dwarf.start_walk(rr ,cc);
-    }
-
-    //start building after reaching 30 wood
-    if(dwarf.lumber() > 30 && start){
-      log << "BUILDING " << endl;
-      log << "Walk to 10 10 " << endl;
-      // if(dwarf.look(10,10) == DWARF || dwarf.look(10,10) == PINE_TREE){
-      //  dwarf.start_walk(10,11);
-      // }else{
-      //  dwarf.start_walk(10, 10); 
-      // }
-      dwarf.start_walk(10,10);
-      start = false;
-      startB = true;
-      return;
-    }
-   
-    if (isNextToAFence(dwarf, r, c) && dwarf.lumber() >= 10) {
-      log << "Found a fence -- build" << endl;
-      if (dwarf.look(r, c+2) == FENCE){
-  dwarf.start_build(EAST);
-  lefty = true;
-  return;
-    }      
-    if (dwarf.look(r, c-2) == FENCE){
-      dwarf.start_build(WEST);
-      righty = true;
-      return;
-    }     
-    if (dwarf.look(r+2, c) == FENCE){
-      dwarf.start_build(SOUTH);
-      up = true;
-      return;
-    }      
-    if (dwarf.look(r-2, c) == FENCE){
-      dwarf.start_build(NORTH);
-      down = true;
-      return;
-    } 
-  }
-  else {    
-    for(int j = 0; j < ROWS; j++){
-      if(isNextToAFence(dwarf, (r+j) % ROWS, (c+j) % COLS) &&
-   !(dwarf.look((r+j) % ROWS, (c+j) % COLS) == FENCE)){
-  rr = (r + j) % ROWS;
-  cc = (c + j) % COLS;
-  log << "Walk to " << rr << " " << cc << endl;
-  dwarf.start_walk(rr,cc);
-  return;
-      }     
-
-      
-      /*
-  Find fences and determine the direction of it.
-  Need to find fences more efficiently 
-  Possibly move all dwarves to center so we have an idea of where to look
-  
-       */
-
-
-      if(isNextToAFence(dwarf, r, (c+j) % COLS) &&
-         !(dwarf.look(r, (c+j) % COLS) == FENCE)){
-        rr = r;
-        cc = (c + j) % COLS;
+  switch(current_section) {
+    case CHOPWOOD: {
+      if (isNextToATree(dwarf, r, c+1)) {
+        // If there is a tree, chop it
+        log << "Found a tree -- chop" << endl;
+        dwarf.start_chop(EAST);
+        return;
+      } else if (isNextToATree(dwarf, r, c-1)) {
+        // If there is a tree, chop it
+        log << "Found a tree -- chop" << endl;
+        dwarf.start_chop(WEST);
+        return;
+      } else if (isNextToATree(dwarf, r+1, c)) {
+        // If there is a tree, chop it
+        log << "Found a tree -- chop" << endl;
+        dwarf.start_chop(SOUTH);
+        return;
+      } else if (isNextToATree(dwarf, r-1, c)) {
+        // If there is a tree, chop it
+        log << "Found a tree -- chop" << endl;
+        dwarf.start_chop(NORTH);
+        return;
+      } else {
+        // Otherwise, move to a random location
+        int rr = 0; int cc = 0;
+        if ((isNextToATree(dwarf, r+1, c+1)) || (isNextToATree(dwarf, r+2, c))) {
+          rr = (r+1);
+          cc = c;
+        } else if ((isNextToATree(dwarf, r-1, c-1)) || (isNextToATree(dwarf, r-2, c))) {
+          rr = (r-1);
+          cc = c;
+        } else if (isNextToATree(dwarf, r, c+2)) {
+          rr = r;
+          cc = (c+1);
+        } else if (isNextToATree(dwarf, r, c-2)) {
+          rr = r;
+          cc = (c-1);
+        } else if (isNextToATree(dwarf, r+2, c+2)) {
+          rr = (r+2);
+          cc = c;
+        } else if (isNextToATree(dwarf, r-2, c-2)) {
+          rr = (r-2);
+          cc = c;
+        } else {
+          rr = rand() % ROWS;
+          cc = rand() % COLS;
+        }
         log << "Walk to " << rr << " " << cc << endl;
-        dwarf.start_walk(rr,cc);
+        dwarf.start_walk(rr, cc);
         return;
       }
-
-      if(isNextToAFence(dwarf, (r+j) % ROWS, c) &&
-         !(dwarf.look((r+j) % ROWS, c) == FENCE)){
-        rr = (r + j) % ROWS;
-        cc = c;
-        log << "Walk to " << rr << " " << cc << endl;
-        dwarf.start_walk(rr,cc);
-        return;
-      }
- 
+      break;
     }
-
+    // Building fences section
+    case BUILDFENCE : {
+      // Check to see if dwarf is in starting position
+      if (setup_check[dwarf.name()] == 0) {
+        current_bs = SETUP_BS;
+      } else {
+        // First chop away trees. If not blocked, build or walk
+        if (isNextToATree(dwarf, r, c+1)) {
+          dwarf.start_chop(EAST);
+        } else if (isNextToATree(dwarf, r, c-1)) {
+          dwarf.start_chop(WEST);
+        } else if (dwarf.look(r, c-1) != FENCE) {
+          current_bs = BUILD_BS;
+        } else if (dwarf.look(r, c-1) == FENCE) {
+          current_bs = WALK_BS;
+        }
+      }
+      // Possible build section action
+      switch(current_bs) {
+        case WALK_BS : {
+          // After building wall, move over
+          dwarf.start_walk((r), (++c));
+          log << dwarf.name() << "/ - Building - WALK" << endl;
+          break;
+        }
+        case BUILD_BS : {
+          if ((r == (dwarf.name() + 5)) && (c < 12)) {
+            // Build wall to the left of every step
+            if (isNextToATree(dwarf, r-1, c)) {
+              dwarf.start_chop(WEST);
+            } else if (isNextToATree(dwarf, r+1, c)) {
+              dwarf.start_chop(EAST);
+            } {
+              dwarf.start_build(WEST);
+            }
+            log << dwarf.name() << "/ - Building - BUILD" << endl;
+          } else {
+            dwarf.start_walk(dwarf.name(), c);
+            log << r << " " << c << "Not in right spot" << endl;
+          }
+          break;
+        }
+        case SETUP_BS : {
+          // Try to walk to starting spot
+          if (isNextToATree(dwarf, dwarf.name() + 5, 5)) {
+              dwarf.start_walk(dwarf.name() + 5, 4);
+              log << dwarf.name() << "-Tree- Move left" << endl;
+            } else if (isNextToATree(dwarf, dwarf.name() + 5, 4)) {
+              dwarf.start_walk(dwarf.name() + 5, 6);
+              log << dwarf.name() << "-Tree- Move right" << endl;
+            } else {
+              dwarf.start_walk(dwarf.name() + 5, 5);
+              log << dwarf.name() << "-Tree- Move left" << endl;
+            }
+            if ((r == dwarf.name() + 5) && (c == 5)) {
+              // If setup is successful, update dwarf
+              setup_check[dwarf.name()] = 1;
+            } else {
+              // If not try to spot left and chop tree
+              dwarf.start_walk(dwarf.name() + 5, 4);
+            }
+            log << dwarf.name() << "/ - Building - SETUP" << endl;
+            break;
+          }
+        }
+        break;
+      }
+    }
   }
-    
-
-    // Otherwise, move to a random location
-    rr = rand() % ROWS;
-    cc = rand() % COLS;
-    log << "RANDOM: Walk to " << rr << " " << cc << endl;
-    dwarf.start_walk(rr, cc);
-    return;
-  }
-}
